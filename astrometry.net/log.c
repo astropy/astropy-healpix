c75f015a7d23afaf5cec073d0f2016d139645efc
Dustin Lang <dstndstn@gmail.com>
2016-10-17 15:26:54 -0400

    import source files from Astrometry.net as of Oct 17, 2016 (post v0.67; 0.67-78-g9dcf0b1)

---
+++ astrometry.net/log.c
@@ -0,0 +1,124 @@
+/*
+# This file is part of the Astrometry.net suite.
+# Licensed under a 3-clause BSD style license - see LICENSE
+*/
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include "log.h"
+
+static log_t g_logger;
+
+static log_t* get_logger() {
+    return &g_logger;
+}
+
+void log_init_structure(log_t* logger, enum log_level level) {
+	logger->level = level;
+    logger->f = stdout;
+	logger->timestamp = FALSE;
+	logger->t0 = timenow();
+	logger->logfunc = NULL;
+	logger->baton = NULL;
+}
+
+void log_init(enum log_level level) {
+	log_init_structure(get_logger(), level);
+}
+
+void log_set_level(enum log_level level) {
+    get_logger()->level = level;
+}
+
+void log_set_timestamp(anbool b) {
+	get_logger()->timestamp = b;
+}
+
+void log_to(FILE* fid) {
+	get_logger()->f = fid;
+}
+
+void log_to_fd(int fd) {
+    // MEMLEAK
+    FILE* fid = fdopen(fd, "a");
+    log_to(fid);
+}
+
+void log_use_function(logfunc_t func, void* baton) {
+	log_t* l = get_logger();
+	l->logfunc = func;
+	l->baton = baton;
+}
+
+log_t* log_create(enum log_level level) {
+	log_t* logger = calloc(1, sizeof(log_t));
+	return logger;
+}
+
+void log_free(log_t* log) {
+	assert(log);
+	free(log);
+}
+
+static void loglvl(const log_t* logger, enum log_level level,
+				   const char* file, int line, const char* func,
+                   const char* format, va_list va) {
+	if (level > logger->level)
+		return;
+	if (logger->f) {
+		if (logger->timestamp)
+			fprintf(logger->f, "[%6i: %.3f] ", (int)getpid(), timenow() - logger->t0);
+		//fprintf(logger->f, "%s:%i ", file, line);
+		vfprintf(logger->f, format, va);
+		fflush(logger->f);
+	}
+	if (logger->logfunc) {
+		logger->logfunc(logger->baton, level, file, line, func, format, va);
+	}
+}
+
+void log_loglevel(enum log_level level,
+				  const char* file, int line, const char* func,
+				  const char* format, ...) {
+    va_list va;
+    va_start(va, format);
+    loglvl(get_logger(), level, file, line, func, format, va);
+    va_end(va);
+}
+
+int log_get_level() {
+    return get_logger()->level;
+}
+
+FILE* log_get_fid() {
+	return get_logger()->f;
+}
+
+#define LOGGER_TEMPLATE(name, level)									\
+	void																\
+	name##_(const log_t* logger, const char* file, int line, const char* func, const char* format, ...) { \
+		va_list va;														\
+		va_start(va, format);											\
+		loglvl(logger, level, file, line, func, format, va);				\
+		va_end(va);														\
+	}																	\
+	void																\
+	name(const char* file, int line, const char* func, const char* format, ...) { \
+		va_list va;														\
+		va_start(va, format);											\
+		loglvl(get_logger(), level, file, line, func, format, va);		\
+		va_end(va);														\
+	}																	\
+	
+LOGGER_TEMPLATE(log_logerr,  LOG_ERROR);
+LOGGER_TEMPLATE(log_logmsg,  LOG_MSG);
+LOGGER_TEMPLATE(log_logverb, LOG_VERB);
+LOGGER_TEMPLATE(log_logdebug,LOG_ALL);
+
+
+
---
+++ astrometry.net/test_healpix-main.c
@@ -0,0 +1,51 @@
+
+
+/* This is auto-generated code. Edit at your own peril. */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "cutest.h"
+
+
+extern void test_side_length(CuTest*);
+extern void test_make_map(CuTest*);
+extern void test_healpix_distance_to_radec(CuTest*);
+extern void test_healpix_neighbours(CuTest*);
+extern void test_big_nside(CuTest*);
+extern void test_distortion_at_pole(CuTest*);
+
+
+void RunAllTests(void) 
+{
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+
+    SUITE_ADD_TEST(suite, test_side_length);
+    SUITE_ADD_TEST(suite, test_make_map);
+    SUITE_ADD_TEST(suite, test_healpix_distance_to_radec);
+    SUITE_ADD_TEST(suite, test_healpix_neighbours);
+    SUITE_ADD_TEST(suite, test_big_nside);
+    SUITE_ADD_TEST(suite, test_distortion_at_pole);
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+
+    printf("%s\n", output->buffer);
+
+    CuSuiteFree(suite);
+    CuStringFree(output);
+}
+
+int main(int argc, char** args)
+{
+    if (argc > 1 && !strcmp(args[1], "-d")) {
+        printf("Setting die on fail.\n");
+        CuDieOnFail();
+    }
+    RunAllTests();
+    return 0;
+}

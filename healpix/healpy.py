93c39caa59df9cc942e98b35423f643ccc2acac5
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-10 23:13:44 +0100

    Update healpy compatibility interface

--- healpix/healpy.py
+++ healpix/healpy.py
@@ -4,41 +4,45 @@ healpy compatible interface.
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__all__ = [
-    'nside2resol',
-    'pix2ang',
-    'ang2pix',
-]
+import numpy as np
+from astropy import units as u
+
+from ._healpix import lonlat_to_healpix, healpix_to_lonlat
+from .core import (n_side_to_resolution, n_side_to_pixel_area,
+                   n_side_to_n_pix, n_pix_to_n_side)
+
+__all__ = ['nside2resol', 'nside2pixarea', 'nside2npix', 'npix2nside',
+           'pix2ang', 'ang2pix']
 
 
 def nside2resol(nside, arcmin=False):
-    import healpy as hp
-    return hp.nside2resol(nside, arcmin)
+    resolution = n_side_to_resolution(nside)
+    if arcmin:
+        return resolution.to(u.arcmin).value
+    else:
+        return resolution.to(u.rad).value
+
+
+def nside2pixarea(nside, degrees=False):
+    area = n_side_to_pixel_area(nside)
+    if degrees:
+        return area.to(u.deg ** 2).value
+    else:
+        return area.to(u.sr).value
+
+
+nside2npix = n_side_to_n_pix
+
+npix2nside = n_pix_to_n_side
 
 
 def pix2ang(nside, ipix, nest=False):
-    """Pixel to angles.
-
-    Parameters
-    ----------
-    nside : int
-        HEALPix ``nside`` parameter
-    ipix : int or array-like
-        HEALPix pixel indices
-    nest : bool
-        Use ``nested`` ordering if True, else ``ring`` ordering.
-
-    Returns
-    -------
-    theta, phi : `~numpy.array`
-        Colatitude and longitude in radians
-    """
-    import healpy as hp
-    theta, phi = hp.pix2ang(nside, ipix, nest)
+    ipix = np.atleast_1d(ipix).astype(np.int32)
+    phi, theta = healpix_to_lonlat(ipix, nside, 1 - int(nest))
     return theta, phi
 
 
 def ang2pix(nside, theta, phi, nest=False):
-    import healpy as hp
-    ipix = hp.ang2pix(nside, theta, phi, nest)
-    return ipix
+    theta = np.atleast_1d(theta)
+    phi = np.atleast_1d(phi)
+    return lonlat_to_healpix(phi, theta, nside, 1 - int(nest))
--- healpix/tests/test_healpy.py
+++ healpix/tests/test_healpy.py
@@ -1,40 +1,35 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
 from numpy.testing import assert_equal, assert_allclose
-from astropy.tests.helper import pytest
-from .. import healpy as hp2
 
-try:
-    import healpy as hp
+from .. import healpy as hp_compat
 
-    HAS_HEALPY = True
-except:
-    HAS_HEALPY = False
+hp = pytest.importorskip('healpy')
 
-
-@pytest.mark.skipif('not HAS_HEALPY')
 def test_nside2resol():
-    actual = hp2.nside2resol(nside=2)
+    actual = hp_compat.nside2resol(nside=2)
     expected = hp.nside2resol(nside=2)
     assert_equal(actual, expected)
 
 
-@pytest.mark.parametrize('nside,theta,phi,nest,ipix', [
-    (256, 0.0000000000000000, 0.0000000000000000, True, 65535),
-    (256, 0.0000000000000000, 1.2566370614359172, True, 65535),
-    (256, 0.0000000000000000, 2.5132741228718345, True, 131071),
-    (256, 0.0000000000000000, 3.7699111843077517, True, 196607),
-    (256, 0.0000000000000000, 5.0265482457436690, True, 262143),
-    (256, 0.0000000000000000, 6.2831853071795862, True, 65535),
-])
-def test_ang2pix(nside, theta, phi, nest, ipix):
-    assert hp2.ang2pix(nside, theta, phi, nest) == ipix
+@pytest.mark.parametrize('nside,theta,phi,nest', [
+    (256, 0.0000000000000000, 0.0000000000000000, True),
+    (256, 0.0000000000000000, 1.2566370614359172, True),
+    (256, 0.0000000000000000, 2.5132741228718345, True),
+    (256, 0.0000000000000000, 3.7699111843077517, True),
+    (256, 0.0000000000000000, 5.0265482457436690, True),
+    (256, 0.0000000000000000, 6.2831853071795862, True)])
+def test_ang2pix(nside, theta, phi, nest):
+    assert hp_compat.ang2pix(nside, theta, phi, nest) == hp.ang2pix(nside, theta, phi, nest)
 
 
-@pytest.mark.parametrize('nside,ipix,nest,theta,phi', [
-    (2, 0, True, 1.230959417340775, 0.785398163397448),
-    (2, 1, True, 0.841068670567930, 1.178097245096172),
-    (2, 2, True, 0.841068670567930, 0.392699081698724),
-])
-def test_pix2ang(nside, ipix, nest, theta, phi):
-    assert_allclose(hp2.pix2ang(nside, ipix, nest), [theta, phi], rtol=1e-10)
+@pytest.mark.parametrize('nside,ipix,nest', [
+    (2, 0, True),
+    (2, 1, True),
+    (2, 2, True)])
+def test_pix2ang(nside, ipix, nest):
+    assert_allclose(hp_compat.pix2ang(nside, ipix, nest),
+                    hp.pix2ang(nside, ipix, nest), rtol=1e-10)

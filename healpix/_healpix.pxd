47951b432dd24a143a17a0ba507e2f76c60b7f87
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-10 11:45:32 +0100

    Added functions for doing conversions to/from nested index

--- healpix/_healpix.pxd
+++ healpix/_healpix.pxd
@@ -1,5 +1,29 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# This file is needed in order to be able to cimport functions into other Cython files
+# This file is needed in order to be able to cimport functions into other Cython
+# files
+
 cdef extern from "healpix.h":
-    int healpix_xy_to_ring(int hp, int Nside)
+
+    # Converts a healpix index from the XY scheme to the RING scheme.
+    int healpix_xy_to_ring(int hp, int Nside);
+
+    # Converts a healpix index from the RING scheme to the XY scheme.
+    int healpix_ring_to_xy(int ring_index, int Nside);
+
+    # Converts a healpix index from the XY scheme to the NESTED scheme.
+    int healpix_xy_to_nested(int hp, int Nside);
+
+    # Converts a healpix index from the NESTED scheme to the XY scheme.
+    int healpix_nested_to_xy(int nested_index, int Nside);
+
+    #  Converts a healpix index, plus fractional offsets (dx,dy), into (x,y,z)
+    #  coordinates on the unit sphere.  (dx,dy) must be in [0, 1].  (0.5, 0.5)
+    #  is the center of the healpix.  (0,0) is the southernmost corner, (1,1) is
+    #  the northernmost corner, (1,0) is the easternmost, and (0,1) the
+    #  westernmost.
+    void healpix_to_radec(int hp, int Nside, double dx, double dy,
+                          double* ra, double* dec);
+
+    # Converts (RA, DEC) coordinates (in radians) to healpix XY index
+    int radectohealpixf(double ra, double dec, int Nside, double* dx, double* dy);
--- healpix/_healpix.pyx
+++ healpix/_healpix.pyx
@@ -4,7 +4,72 @@ import cython
 
 ctypedef np.double_t DOUBLE_T
 
-from _healpix cimport healpix_xy_to_ring
 
-def test():
-    return healpix_xy_to_ring(1, 2)
+def nested_to_lonlat(np.ndarray[int, ndim=1, mode="c"] nested_index, int n_side):
+
+    cdef int n = nested_index.shape[0]
+    cdef double dx, dy;
+    cdef int xy_index
+    cdef np.ndarray[double, ndim=1, mode="c"] lon = np.zeros(n, dtype=np.double)
+    cdef np.ndarray[double, ndim=1, mode="c"] lat = np.zeros(n, dtype=np.double)
+
+    dx = 0
+    dy = 0
+
+    for i in range(n):
+        xy_index = healpix_nested_to_xy(nested_index[i], n_side)
+        healpix_to_radec(xy_index, n_side, dx, dy, &lon[i], &lat[i])
+    return lon, lat
+
+
+def nested_with_offset_to_lonlat(np.ndarray[int, ndim=1, mode="c"] nested_index,
+                                 np.ndarray[double, ndim=1, mode="c"] dx,
+                                 np.ndarray[double, ndim=1, mode="c"] dy,
+                                 int n_side):
+
+    cdef int n = nested_index.shape[0]
+    cdef int xy_index
+    cdef np.ndarray[double, ndim=1, mode="c"] lon = np.zeros(n, dtype=np.double)
+    cdef np.ndarray[double, ndim=1, mode="c"] lat = np.zeros(n, dtype=np.double)
+
+    dx = 0
+    dy = 0
+
+    for i in range(n):
+        xy_index = healpix_nested_to_xy(nested_index[i], n_side)
+        healpix_to_radec(xy_index, n_side, dx[i], dy[i], &lon[i], &lat[i])
+
+    return lon, lat
+
+
+def lonlat_to_nested(np.ndarray[double, ndim=1, mode="c"] lon,
+                     np.ndarray[double, ndim=1, mode="c"] lat,
+                     int n_side):
+
+    cdef int n = lon.shape[0]
+    cdef int xy_index
+    cdef double dx, dy;
+    cdef np.ndarray[int, ndim=1, mode="c"] nested_index = np.zeros(n, dtype=np.int32)
+
+    for i in range(n):
+        xy_index = radectohealpixf(lon[i], lat[i], n_side, &dx, &dy)
+        nested_index[i] = healpix_xy_to_nested(xy_index, n_side)
+
+    return nested_index
+
+
+def lonlat_to_nested_with_offset(np.ndarray[double, ndim=1, mode="c"] lon,
+                                 np.ndarray[double, ndim=1, mode="c"] lat,
+                                 int n_side):
+
+    cdef int n = lon.shape[0]
+    cdef int xy_index
+    cdef np.ndarray[int, ndim=1, mode="c"] nested_index = np.zeros(n, dtype=np.int32)
+    cdef np.ndarray[double, ndim=1, mode="c"] dx = np.zeros(n, dtype=np.double)
+    cdef np.ndarray[double, ndim=1, mode="c"] dy = np.zeros(n, dtype=np.double)
+
+    for i in range(n):
+        xy_index = radectohealpixf(lon[i], lat[i], n_side, &dx[i], &dy[i])
+        nested_index[i] = healpix_xy_to_nested(xy_index, n_side)
+
+    return nested_index, dx, dy

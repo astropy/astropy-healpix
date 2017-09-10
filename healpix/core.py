f0931dafc52feaab3f73cfe4b14b81e12b175f58
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-10 20:32:57 +0100

    Implement n_side_to_pixel_area

--- healpix/core.py
+++ healpix/core.py
@@ -3,7 +3,27 @@ from __future__ import print_function, division
 import math
 from astropy import units as u
 
-__all__ = ['n_side_to_resolution']
+__all__ = ['n_side_to_pixel_area', 'n_side_to_resolution']
+
+
+def n_side_to_pixel_area(n_side):
+    """
+    Find the area of healpix pixels given the pixel dimensions of one of
+    the 12 'top-level' healpix tiles.
+
+    Parameters
+    ----------
+    n_side : int
+        The number of pixels on the side of one of the 12 'top-level' healpix tiles.
+
+    Returns
+    -------
+    pixel_area : :class:`~astropy.units.Quantity`
+        The area of the healpix pixels
+    """
+    n_pix = 12 * n_side * n_side
+    pixel_area = 4 * math.pi / n_pix * u.sr
+    return pixel_area
 
 
 def n_side_to_resolution(n_side):
@@ -21,6 +41,4 @@ def n_side_to_resolution(n_side):
     resolution : :class:`~astropy.units.Quantity`
         The resolution of the healpix pixels
     """
-    n_pix = 12 * n_side * n_side
-    pixel_area = 4 * math.pi / n_pix * u.sr
-    return (pixel_area ** 0.5).to(u.arcmin)
+    return (n_side_to_pixel_area(n_side) ** 0.5).to(u.arcmin)
--- healpix/tests/test_core.py
+++ healpix/tests/test_core.py
@@ -1,7 +1,13 @@
 from numpy.testing import assert_allclose
 from astropy import units as u
 
-from ..core import n_side_to_resolution
+from ..core import n_side_to_pixel_area, n_side_to_resolution
+
+
+def test_n_side_to_pixel_area():
+    resolution = n_side_to_pixel_area(256)
+    assert_allclose(resolution.value, 1.5978966540475428e-05)
+    assert resolution.unit == u.sr
 
 
 def test_n_side_to_resolution():

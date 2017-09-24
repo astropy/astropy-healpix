494d27849bf41c8f63edd9130f36456669a7053d
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-24 17:43:21 +0100

    Added nest2ring and ring2nest and properly implement lonlat for pix2ang and ang2pix

--- healpix/healpy.py
+++ healpix/healpy.py
@@ -11,7 +11,10 @@ from astropy import units as u
 
 from .core_cython import lonlat_to_healpix, healpix_to_lonlat
 from .core import (nside_to_pixel_resolution, nside_to_pixel_area,
-                   nside_to_npix, npix_to_nside, level_to_nside)
+                   nside_to_npix, npix_to_nside, nested_to_ring, ring_to_nested,
+                   level_to_nside)
+
+RAD2DEG = 180 / np.pi
 
 __all__ = ['nside2resol', 'nside2pixarea', 'nside2npix', 'npix2nside',
            'pix2ang', 'ang2pix', 'order2nside']
@@ -45,13 +48,36 @@ def order2nside(order):
     return level_to_nside(order)
 
 
-def pix2ang(nside, ipix, nest=False):
+def pix2ang(nside, ipix, nest=False, lonlat=False):
     ipix = np.atleast_1d(ipix).astype(np.int64, copy=False)
     lon, lat = healpix_to_lonlat(ipix, nside, 1 - int(nest))
-    return np.pi / 2 - lat, lon
+    # We use in-place operations below to avoid making temporary arrays - this
+    # is safe because the lon/lat arrays returned from healpix_to_lonlat are
+    # new and not used elsewhere.
+    if lonlat:
+        lon *= RAD2DEG
+        lat *= RAD2DEG
+        return lon, lat
+    else:
+        np.subtract(0.5 * np.pi, lat, out=lat)
+        return lat, lon
 
 
-def ang2pix(nside, theta, phi, nest=False):
-    lat = np.pi / 2. - np.atleast_1d(theta)
-    lon = np.atleast_1d(phi)
+def ang2pix(nside, theta, phi, nest=False, lonlat=False):
+    # Unlike in pix2ang, we don't use in-place operations since we don't
+    # want to modify theta and phi since the user may be using them elsewhere.
+    if lonlat:
+        lon, lat = np.atleast_1d(theta) / RAD2DEG, np.atleast_1d(phi) / RAD2DEG
+    else:
+        lat, lon = np.pi / 2. - np.atleast_1d(theta), np.atleast_1d(phi)
     return lonlat_to_healpix(lon, lat, nside, 1 - int(nest))
+
+
+def nest2ring(nside, ipix):
+    ipix = np.atleast_1d(ipix).astype(np.int64, copy=False)
+    return nested_to_ring(ipix, nside)
+
+
+def ring2nest(nside, ipix):
+    ipix = np.atleast_1d(ipix).astype(np.int64, copy=False)
+    return ring_to_nested(ipix, nside)

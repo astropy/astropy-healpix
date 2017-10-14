1cc01eda3508584d314a2b50c5626518624a33e0
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-15 00:07:25 +0100

    Improve formatting of new C code

--- astropy_healpix/interpolation.h
+++ astropy_healpix/interpolation.h
@@ -1,6 +1,6 @@
 void interpolate_weights(double lon, double lat, int64_t *ring_indices,
-												 double *weights, int Nside);
+                         double *weights, int Nside);
 
-void ring_neighbours(double lon, double lat, int Nside,
-									   int64_t *ring1, int64_t *ring2,
-										 double *lon1, double *lat1, double *lon2, double *lat2);
+void ring_neighbours(double lon, double lat, int Nside, int64_t *ring1,
+                     int64_t *ring2, double *lon1, double *lat1, double *lon2,
+                     double *lat2);

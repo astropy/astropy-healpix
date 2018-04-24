75f27b259113781289da7ac7303217d282adfb69
James Noss <jnoss@stsci.edu>
2018-04-24 10:20:03 -0400

    Add header guard to interpolation.h
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- astropy_healpix/interpolation.h
+++ astropy_healpix/interpolation.h
@@ -1,3 +1,6 @@
+#ifndef ASTROPY_HEALPIX_INTERPOLATION_INCL
+#define ASTROPY_HEALPIX_INTERPOLATION_INCL
+
 #ifdef _MSC_VER
 #if _MSC_VER >= 1600
 #include <stdint.h>
@@ -10,3 +13,5 @@
 
 void interpolate_weights(double lon, double lat, int64_t *ring_indices,
                          double *weights, int Nside);
+
+#endif

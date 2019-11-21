cbdc472e4e7558a90c0c4bb8f2a6a9dfe6c6d88a
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-21 14:43:08 +0000

    Updated and simplified conftest.py

--- astropy_healpix/conftest.py
+++ astropy_healpix/conftest.py
@@ -1,65 +1,37 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# this contains imports plugins that configure py.test for astropy tests.
-# by importing them here in conftest.py they are discoverable by py.test
-# no matter how it is invoked within the source tree.
+import os
+
+import numpy as np
 
 from astropy.version import version as astropy_version
 if astropy_version < '3.0':
-    # With older versions of Astropy, we actually need to import the pytest
-    # plugins themselves in order to make them discoverable by pytest.
     from astropy.tests.pytest_plugins import *
+    del pytest_report_header
 else:
-    # As of Astropy 3.0, the pytest plugins provided by Astropy are
-    # automatically made available when Astropy is installed. This means it's
-    # not necessary to import them here, but we still need to import global
-    # variables that are used for configuration.
-    from astropy.tests.plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 
 from astropy.tests.helper import enable_deprecations_as_exceptions
 
-# Uncomment the following line to treat all DeprecationWarnings as
-# exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
-# as follow (although default should work for most cases).
-# To ignore some packages that produce deprecation warnings on import
-# (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
-# 'setuptools'), add:
-#     modules_to_ignore_on_import=['module_1', 'module_2']
-# To ignore some specific deprecation warning messages for Python version
-# MAJOR.MINOR or later, add:
-#     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
 enable_deprecations_as_exceptions()
 
-# Uncomment and customize the following lines to add/remove entries from
-# the list of packages for which version numbers are displayed when running
-# the tests. Making it pass for KeyError is essential in some cases when
-# the package uses other astropy affiliated packages.
-try:
+
+def pytest_configure(config):
+
+    config.option.astropy_header = True
+
+    PYTEST_HEADER_MODULES.pop('h5py', None)
+    PYTEST_HEADER_MODULES.pop('Pandas', None)
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     PYTEST_HEADER_MODULES['healpy'] = 'healpy'
-    PYTEST_HEADER_MODULES['Cython'] = 'cython'
-    del PYTEST_HEADER_MODULES['h5py']
-    del PYTEST_HEADER_MODULES['Pandas']
-except (NameError, KeyError):  # NameError is needed to support Astropy < 1.0
-    pass
 
-# Uncomment the following lines to display the version number of the
-# package rather than the version number of Astropy in the top line when
-# running the tests.
-import os
+    from .version import version, astropy_helpers_version
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version
+    TESTED_VERSIONS['astropy_helpers'] = astropy_helpers_version
 
-# This is to figure out the affiliated package version, rather than
-# using Astropy's
-from .version import version, astropy_helpers_version
-
-packagename = os.path.basename(os.path.dirname(__file__))
-TESTED_VERSIONS[packagename] = version
-TESTED_VERSIONS['astropy_helpers'] = astropy_helpers_version
-
-def pytest_configure():
-    """Set the Numpy print style to a fixed version to make doctest outputs
-    reproducible."""
-    import numpy as np
+    # Set the Numpy print style to a fixed version to make doctest outputs
+    # reproducible.
     try:
         np.set_printoptions(legacy='1.13')
     except TypeError:

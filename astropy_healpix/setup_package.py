120f0d5b357c97fe6525d58be4d474bdd30e53b4
Leo Singer <leo.singer@ligo.org>
2025-01-31 16:54:42 -0500

    Modernize supported Python and Numpy versions

--- astropy_healpix/setup_package.py
+++ astropy_healpix/setup_package.py
@@ -38,7 +38,7 @@ def get_extensions():
         language="c",
         extra_compile_args=['-O2'],
         py_limited_api=True,
-        define_macros=[('Py_LIMITED_API', 0x03090000),
+        define_macros=[('Py_LIMITED_API', 0x030A0000),
                        ('NPY_TARGET_VERSION', 'NPY_1_19_API_VERSION'),
                        ('NPY_NO_DEPRECATED_API', 'NPY_1_19_API_VERSION')])
 

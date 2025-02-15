bc1f9d80a7fc33fcad47a5f7de4f674a7d17280f
Leo Singer <leo.singer@ligo.org>
2025-02-14 22:18:41 -0500

    Remove obsolete _astropy_init.py
    Fixes #248.

--- astropy_healpix/__init__.py
+++ astropy_healpix/__init__.py
@@ -4,11 +4,10 @@
 BSD-licensed HEALPix for Astropy.
 """
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
-
 from .high_level import *  # noqa
 from .core import *  # noqa
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
